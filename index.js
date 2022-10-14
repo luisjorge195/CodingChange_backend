16c06ee702bfd968ab1adf4c18ac3e758a38fa9a
Jorge Pedroza <sebastian.tobon@corprevenir.com>
2022-10-14 14:40:56 -0500

    modificacion

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ import cors from 'cors'
 dotenv.config();
 
 const app = express();
-// const whiteList = [process.env.FRONTEND_URL]
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//       if(whiteList.includes(origin)){
-//           callback(null, true)
-//           return
-//       }
-//         callback(new Error('Not allowed by CORS'))
-//     }
-// }
-// 
-// app.use(cors(corsOptions))
+const whiteList = [process.env.FRONTEND_URL]
+const corsOptions = {
+    origin: function (origin, callback) {
+      if(whiteList.includes(origin)){
+          callback(null, true)
+          return
+      }
+        callback(new Error('Not allowed by CORS'))
+    }
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/museum', rutas)
